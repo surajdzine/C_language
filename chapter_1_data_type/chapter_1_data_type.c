0feee3f499d258e8f8a53b83ee8220d2d3244699
surajdzine <surajdzine@gmail.com>
2026-03-07 11:17:26 +0530

    input command

--- chapter_1_data_type/chapter_1_data_type.c
+++ chapter_1_data_type/chapter_1_data_type.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 int main(){
-    printf("suraj singh rawat");
+    printf("suraj singh rawat \n");
+    // input case 
+    int age;
+    printf("what is your age :");
+    scanf("%d",&age);
+    printf("so your age is %d",age);
     return 0;
 }
 
 // this is the basic structure 
-/*first you have to install a compiller */
-
+/*first you have to install a compiller
+variables are the name of the memory location which stores some data 
+*/
+// in this you you give input as your age 
