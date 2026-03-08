0fd2b1cd210ee54ed68da6b87cb2ac998cb34754
surajdzine <surajdzine@gmail.com>
2026-03-08 23:39:05 +0530

    new exercise

---
+++ chapter_1_data_type/exercise1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    //first value 
+    printf("enter your first number :");
+    scanf("%d",&a);
+    // second value 
+    printf("enter your second number :");
+    scanf("%d",&b);
+    int sum = a + b;
+    printf("sum of a nd b %d",sum);
+    return 0;
+}
+
+/* code for the addition of two number */
